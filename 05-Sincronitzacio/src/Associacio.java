256969bf463c31fd284d159f17c8c8895708cc06
Pau Pich <pichvives3@gmail.com>
2025-01-27 18:38:41 +0100

    05-acabat

--- 05-Sincronitzacio/src/Associacio.java
+++ 05-Sincronitzacio/src/Associacio.java
@@ -7,7 +7,31 @@ public class Associacio {
         this.compte = Compte.getInstancia();
         this.socis = new Soci[numSocis];
 
-        for()
+        for(int i = 0; i < numSocis; i++) {
+            socis[i] = new Soci(compte);
+        }
+    }
+    //iniciar els fils de socis
+    public void iniciaCompteTempsSocis() {
+        Thread[] fils = new Thread[numSocis];
+
+        for (int i = 0; i < numSocis; i++) {
+            fils[i] = new Thread(socis[i]); //crear un fil per cada soci
+            fils[i].start();
+        }
+
+        //esperar que tots els fils acabin
+        for (Thread fil : fils) {
+            try {
+                fil.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
+    //mostrar saldo final del compte
+    public void mostraBalancComptes() {
+        System.out.printf("Saldo: %.2f%n", compte.getSaldo()); // Mostrem el saldo final
+    }
 }
--- 05-Sincronitzacio/src/Soci.java
+++ 05-Sincronitzacio/src/Soci.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Soci extends Runnable {
+public class Soci implements Runnable {
     private final Compte compte;
     private final float aportacio = 10f; 
     private final int esperaMax = 100;
@@ -13,6 +13,21 @@ public class Soci extends Runnable {
 
     @Override
     public void run() {
+        for (int any = 0; any < maxAnys; any++) { // pels anys
+            for (int mes = 1; mes <= 12; mes++) { // pels mesos
+                if (mes % 2 == 0) {
+                    compte.actualitzaSaldo(aportacio);
+                } else {
+                    compte.actualitzaSaldo(-aportacio);
+                }
 
+                // Simular temps d'espera aleatori
+                try {
+                    Thread.sleep(random.nextInt(esperaMax));
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
